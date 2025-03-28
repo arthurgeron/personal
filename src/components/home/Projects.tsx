56c31818180b5c4fdd8b1ca39c1d5779d384f2bd
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2025-03-28 13:27:21 -0300

    feat: improve separator design

--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -58,20 +58,10 @@ export default function Projects() {
   return (
     <>
       <div class="section-divider">
-        <svg 
-          class="section-divider-wave" 
-          xmlns="http://www.w3.org/2000/svg" 
-          viewBox="0 0 1440 120"
-          preserveAspectRatio="none"
+        <div 
+          class="section-divider-gradient" 
           aria-hidden="true"
-        >
-          <title>Section Divider Wave</title>
-          <path 
-            fill="rgb(229 231 235)" 
-            fill-opacity="1" 
-            d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,80C672,64,768,64,864,64C960,64,1056,64,1152,58.7C1248,53,1344,43,1392,37.3L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-          />
-        </svg>
+        />
       </div>
       <section ref={containerRef} class="section bg-base-200 py-20">
         <div class="container-custom">
